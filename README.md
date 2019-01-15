de9d44d6a41334596c0b1da74410a0f07bc70464
Lukas Neumann <lksnmnn@gmail.com>
2019-01-15 11:36:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Auto semver
+This shell script automates the process of bumping a version number in json formatted version file following semver standard (major, minor, patch).
+
+## Usage
+`./automatic-semver-version.sh -p <path/to/version.json> -b <major|minor|patch>`
+
+## What it does
+- reads old version from file (default "version.json")
+- gets recommended bump type reading commit messages (using conventional-recommended-bump)
+- bumps the version accordingly
+- writes new version back to file
+- updates CHANGELOG.md (using conventional-changelog)
+- creates a version commit
+- tags the commit with the new version 
+
+## What it needs to run
+- bash (to run the script)
+- jq (to parse JSON)
+- [conventional-changelog](https://github.com/conventional-changelog/conventional-changelog) (to create the changelog)
+- [conventional-recommended-bump](https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-recommended-bump) (to get the default bump type, if not supplied)
+- git (to commit and tag)
+
+## Helper
+There is a package.json which installs the above mentioned JavaScript dependencies.
+Just call `npm install`.
+
+## License
+Free software, see LICENSE file for further information.
---
+++ automatic_semver_version.sh
@@ -0,0 +1,121 @@
+#! /bin/bash
+
+##
+# This script is used to for automatic versioning following the semver standard. 
+# Basically, it should work like NPMs `npm version <major|minor|patch>` 
+#
+# If you run this script, it will automatically bump the version in the specified
+# version.json file (e.g. package.json, composer.json, ...).
+# 
+# If no <bump type> is supplied, it will use `conventional-recommended-bump -p 'angular'`
+# to find the recommended next version. 
+
+# After increasing the version, it updates the CHANGELOG.md, creates a new commit and
+# git tags the commit with the new version number. Push has to be made manually.
+#
+# Usage: ./automatic_semver_version.sh -p </path/to/version.json> -b <major|minor|patch>
+##
+
+# init vars
+use_default_path=true
+verbose=false
+dry_run=false
+
+# read in args (version.json path, bump type)
+while getopts ':vdp:b:' OPTION
+do
+  case "$OPTION" in
+    p)  version_path="$OPTARG"
+        use_default_path=false
+    ;;
+    v)  echo "Verbose execusion"
+        verbose=true
+    ;;
+    d)  echo "Dry run. No changes will be applied."
+        dry_run=true
+    ;;
+    b)  echo "Using supplied bump type."
+        bump_type="$OPTARG"
+    ;;
+    *)  echo "Ignoring unknown parameter."
+  esac
+done
+
+# set default path
+if [ "$use_default_path" == true ]
+  then
+    version_path="version.json"
+fi
+
+# recommended semver bump type (i.e. major, minor, patch, hotfix)
+if [ -z "$bump_type" ]
+  then
+    bump_type=$( conventional-recommended-bump -p 'angular' )
+fi
+if [ "$verbose" == true ]
+  then
+    echo "Bump type is ${bump_type}"
+fi
+
+# read the old version from file ({}.version)
+old_version=$(jq -r '.version' ${version_path})
+# check if version is set, else set 0.0.0
+if [ -z ${old_version+x} ]
+  then
+    old_version="0.0.0"
+fi
+if [ "$verbose" == true ]
+  then
+    echo "Old version is ${old_version}"
+fi
+
+# explode version string into array
+version_array=(${old_version//./ })
+
+# increment version numbers as requested
+case $bump_type in
+  patch)
+    ((a[2]++))
+    break;;
+  minor)
+    ((a[1]++))
+    a[2]=0 
+    break;; 
+  major)
+    ((a[0]++))
+    a[1]=0 
+    a[2]=0 
+    break;; 
+esac
+
+# combine to new version
+new_version="${a[0]}.${a[1]}.${a[2]}"
+if [ "$verbose" == true ]
+  then
+    echo "New version is ${new_version}"
+fi
+
+# write back new version in version file
+updated_file=$(jq '.version = $new' --arg new "${new_version}" ${version_path})
+echo "$new_file" > $version_path
+
+# add version file to git
+git add ${version_path}
+if [ "$verbose" == true ]
+  then
+    echo "Updated version file"
+fi
+
+# generate CHANGELOG.md
+conventional-changelog -p angular -k "${version_path}" -i CHANGELOG.md -s
+git add CHANGELOG.md
+if [ "$verbose" == true ]
+  then
+    echo "Updated CHANGELOG.md"
+fi
+
+# commit current changes
+git commit -am "chore(version): create new ${bump_type} version ${new_version}"
+
+# tag the current commit
+git tag -a v"${new_version}" -m "Automatic version: ${bump_type}"
